66c5777af6ff3e171f5fa80b792e993b7cabfafc
SanoP <sanodeemantha@gmail.com>
2020-07-22 11:11:57 +0530

    Update Classes

--- AppInitilaizer.java
+++ AppInitilaizer.java
@@ -1,4 +1,5 @@
 class AppInitializer{
     public static void main(String args[]){
+        Student s = new Student(25,"Sanoj");
     }
 }
--- Student.java
+++ Student.java
@@ -1,2 +1,25 @@
 class Student{
+    private int id;
+    private String name;
+
+    Student(int id,String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public void setID(int id){
+        this.id = id;
+    }
+
+    public int getID(){
+        return id;
+    }
+    
+    public void setname(int name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
 }
